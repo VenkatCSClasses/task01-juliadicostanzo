16ff2bfa82a44eeb92afc18f17298cd180bca2c2
juliadicostanzo <jdicostanzo@ithaca.edu>
2026-01-28 10:51:20 -0500

    added isEmailValidTest() tests

--- src/test/java/edu/ithaca/dturnbull/bank/BankAccountTest.java
+++ src/test/java/edu/ithaca/dturnbull/bank/BankAccountTest.java
@@ -25,7 +25,19 @@ class BankAccountTest {
     @Test
     void isEmailValidTest(){
         assertTrue(BankAccount.isEmailValid( "a@b.com"));   // valid email address
-        assertFalse( BankAccount.isEmailValid(""));         // empty string
+        assertTrue(BankAccount.isEmailValid( "a@b.org"));   // valid email address
+        assertTrue(BankAccount.isEmailValid( "a-b@c.com"));   // valid email address
+        assertFalse(BankAccount.isEmailValid(""));         // empty string
+        assertFalse(BankAccount.isEmailValid("a@.com"));   // missing second-level domain name
+        assertFalse(BankAccount.isEmailValid("a@b"));      // missing top-level domain name
+        assertFalse(BankAccount.isEmailValid("@b.com"));   // missing local part (username)
+        assertFalse(BankAccount.isEmailValid("ab.com"));   // missing @ symbol
+        assertFalse(BankAccount.isEmailValid("a@b..com")); // double dot in domain name
+        assertFalse(BankAccount.isEmailValid("-@b.com"));  // local part starts with invalid character
+        assertFalse(BankAccount.isEmailValid("a@b#.com")); // invalid character in domain name
+        
+
+
 
         
     }
